f2b092fd2160788d9ccf4ac95b060cacb03a1438
ayushThakur747 <66708059+ayushThakur747@users.noreply.github.com>
2021-07-26 10:55:03 +0530

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,5 @@
 const request = require('request');
 
-//geocoding: it is the process of taking the name of the place and converting it into latitudenal and logitudenal quadrinates
 const geocode = (address,callback)=>{
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address)+ '.json?access_token=pk.eyJ1IjoiYXl1c2g3NDciLCJhIjoiY2tyaXA1ZDd5MHZrbTJucGdxbmN1MWpjMSJ9.cHPvocADOfl67TT0oSAINA'; //encodeUIR method is for situation if we want special char in our address
 
